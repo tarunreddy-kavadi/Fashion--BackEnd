cd58689117f1b34871eb0967673fae1e661f06a4
Tarun Reddy Kavadi <tarunreddy.kavadi.79@gmail.com>
2023-03-09 19:02:01 +0000

    small change

--- index.js
+++ index.js
@@ -22,10 +22,11 @@ mongoose
 
   const corsOptions = {
     origin: 'https://quiet-kulfi-8dacf7.netlify.app',
-    optionsSuccessStatus: 200 
+    optionsSuccessStatus: 200,
+    credentials: true
   }
   
-  app.use(cors(corsOptions))
+app.use(cors(corsOptions))
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
@@ -35,6 +36,12 @@ app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
 
+app.use((req,res,next)=>{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+})
 
 app.get("/",(req,res)=>{
 res.send("Hello its Working")
