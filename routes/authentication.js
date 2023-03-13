e05f71bfafa1e578c998dd955230be637d63112f
Tarun Reddy Kavadi <tarunreddy.kavadi.79@gmail.com>
2023-03-13 09:02:19 +0000

    c

--- routes/authentication.js
+++ routes/authentication.js
@@ -26,7 +26,7 @@ router.post("/register", async (req, res) => {
 
 router.post("/login", async (req, res) => {
   try {
-    const user = await User.find({ username: req.body.username });
+    const user = await User.findOne({ username: req.body.username });
     if(!user){
       return res.status(401).json('user not found')
     }
